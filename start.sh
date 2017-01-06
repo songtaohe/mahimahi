808484450d6012246ed7e147fe3353174ace3600
Songtao He <songtaohe0@mit.edu>
2017-01-05 23:36:46 -0500

    local

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 xterm -geometry 93x31+300+150 -e "iperf -c 100.64.0.1 -i 2 -t 10000 -N" &
 xterm -geometry 93x31+200+150 -e "ping 100.64.0.1"&
-xterm -geometry 233x31+500+650 -hold -e "cd ../rl-qm/mahimahiSocketInterfaceSingleLoop/; python RLMahimahiInterface.py 100.64.0.4" &
+xterm -geometry 233x31+500+650 -hold -e "cd ../rl-qm/rl-framework/; ./run.sh 100.64.0.4" &
 xterm &
 xterm
 pkill xterm
