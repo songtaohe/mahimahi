103451a9bb1031d739e613a49df09c22c3281623
Songtao He <songtaohe0@mit.edu>
2017-01-13 17:17:26 -0500

    sync

--- TestPowerScore.sh
+++ TestPowerScore.sh
@@ -7,7 +7,7 @@ do
 for flow in 1 10
 do
 
-#timeout 150s mm-delay ${delay} mm-link traces/Verizon-LTE-short.up trace14000 --uplink-queue=pie --downlink-queue=infinite --uplink-queue-args="packets=200, qdelay_ref=20, max_burst=1" --uplink-log=uplinklog/PIE${delay}ms_${flow} --meter-uplink ./iperf.sh ${flow}
+timeout 150s mm-delay ${delay} mm-link traces/Verizon-LTE-short.up trace14000 --uplink-queue=pie --downlink-queue=infinite --uplink-queue-args="packets=200, qdelay_ref=20, max_burst=1" --uplink-log=uplinklog/PIE${delay}ms_${flow} --meter-uplink ./iperf.sh ${flow}
 
 echo PIE${delay}ms_${flow}
 mm-throughput-graph 500 uplinklog/PIE${delay}ms_${flow} > /dev/null
@@ -22,7 +22,7 @@ do
 for flow in 1 10
 do
 
-#timeout 150s mm-delay ${delay} mm-link traces/Verizon-LTE-short.up trace14000 --uplink-queue=codel --downlink-queue=infinite --uplink-queue-args="packets=200, target=5, interval=100" --uplink-log=uplinklog/Codel${delay}ms_${flow} --meter-uplink ./iperf.sh ${flow}
+timeout 150s mm-delay ${delay} mm-link traces/Verizon-LTE-short.up trace14000 --uplink-queue=codel --downlink-queue=infinite --uplink-queue-args="packets=200, target=5, interval=100" --uplink-log=uplinklog/Codel${delay}ms_${flow} --meter-uplink ./iperf.sh ${flow}
 
 echo Codel${delay}ms_${flow}
 mm-throughput-graph 500 uplinklog/Codel${delay}ms_${flow} > /dev/null
