c261a1b91b983aea2f8cad0abaa3ebd4f68b6123
Songtao He <songtaohe0@mit.edu>
2017-02-17 11:51:47 -0500

    Sync

--- TestPie.sh
+++ TestPie.sh
@@ -1,7 +1,7 @@
-sudo sysctl -w net.ipv4.ip_forward=1
+#sudo sysctl -w net.ipv4.ip_forward=1
 #mm-link trace10 trace10 --uplink-queue=droptail --downlink-queue=infinite --uplink-queue-args="packets=10000"
 #mm-delay $1 mm-link $2 $3 --uplink-queue=pie --once --uplink-log=uplink_log --downlink-queue=infinite --uplink-queue-args="packets=1000, qdelay_ref=$5, max_burst=1" ./start2L.sh $4 #xterm -e "cd ../rl-qm/mahimahiSocketInterfaceSingleLoop; python RLMahimahiInterface.py 100.64.0.4" &
-mm-delay $1 mm-link $2 $3 --uplink-queue=pie --downlink-queue=infinite --uplink-queue-args="packets=200, qdelay_ref=$5, max_burst=1" --uplink-log=uplink_log --meter-uplink ./start2L.sh $4 #xterm -e "cd ../rl-qm/mahimahiSocketInterfaceSingleLoop; python RLMahimahiInterface.py 100.64.0.4" &
+mm-delay $1 mm-link $2 $3 --uplink-queue=pie --downlink-queue=infinite --uplink-queue-args="packets=200, qdelay_ref=$5, max_burst=1" --uplink-log=$6 --meter-uplink ./start2L.sh $4 #xterm -e "cd ../rl-qm/mahimahiSocketInterfaceSingleLoop; python RLMahimahiInterface.py 100.64.0.4" &
 #mm-delay $1 mm-link $2 $3 --uplink-queue=pie --downlink-queue=infinite --uplink-queue-args="packets=200, qdelay_ref=$5, max_burst=1" --meter-uplink ./start2L.sh $4 #xterm -e "cd ../rl-qm/mahimahiSocketInterfaceSingleLoop; python RLMahimahiInterface.py 100.64.0.4" &
 #mm-link trace10 trace10 --uplink-queue=infinite --downlink-queue=pie --downlink-queue-args="packets=300, qdelay_ref=50, max_burst=1" mm-delay 10
 
--- start2L.sh
+++ start2L.sh
@@ -1,4 +1,5 @@
 xterm -geometry 93x11+30+15 -e "iperf -c 100.64.0.1 -i 2 -t 100000 -N -P $1" &
+#xterm -geometry 93x11+60+30 -e "iperf -c 100.64.0.1 -u -b 40k -l 100 -t 100000 -i 2" &
 xterm -geometry 93x11+90+15 -e "ping 100.64.0.1"&
 xterm -geometry 233x21+50+85 -hold -e "cd ../rl-qm/rl-framework/; ./run.sh 100.64.0.4" &
 xterm &
